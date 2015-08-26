5f79a4aca117b65825db93e2734485f737bb0a54
Sebastian Engelmann <s.engelmann@fu-berlin.de>
2015-08-26 14:23:56 +0200

    Loeschen der Datei

--- tests/testthat/test_fitlnorm.R
+++
@@ -1,6 +0,0 @@
-test_that("fitlnorm_correctness", {
-  x <- sample(x = 1:300, size = 500, replace = TRUE)
-  result <- MASS::fitdistr(x = x, densfun = "lognormal")
-  expect_that(fitlnorm(x = x)[1], equals(result$estimate[1]))
-  expect_that(fitlnorm(x = x)[2], equals(result$estimate[2]))
-})
