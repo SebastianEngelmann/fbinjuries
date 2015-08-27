232e4b2a03a9cd60be342b4a4874b2ab2166394a
Sebastian Engelmann <s.engelmann@fu-berlin.de>
2015-08-27 13:36:23 +0200

    Aktualisierung der Dokumentation

--- R/fplayer.R
+++ R/fplayer.R
@@ -1,12 +1,17 @@
 #' Find a set of players
 #'
-#' @param position player position; \code{position = "TW"} (goalkeeper);
+#' @param position player position:
+#'
+#' \code{position = "TW"} (goalkeeper);
+#'
 #' \code{position = "AW"} (defense);
+#'
 #' \code{position = "MF"} (midfield);
+#'
 #' \code{position = "ST"} (attack)
 #' @param minsickdays the number of minimal sickdays in the past
 #' @param maxsickdays the number of maximal sickdays in the past
-#' @param data data.frame which contains the data of fbinjuries
+#' @param data data frame which contains the data of fbinjuries
 #' @rdname fplayer
 #' @export
 #' @examples
